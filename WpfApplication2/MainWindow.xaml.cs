c3588691b036b2b8733c001d05f337ed054f5123
alfik006 <anneliesse15@gmail.com>
2015-11-02 20:41:38 +0100

    Zahlen skalierbar

--- WpfApplication2/MainWindow.xaml.cs
+++ WpfApplication2/MainWindow.xaml.cs
@@ -70,5 +70,92 @@ namespace WpfApplication2
             txtPhone.Text = newText;
 
         }
+
+        private void windowframe_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //Set dynamic object size
+            if (40 - (420 - windowframe.Height) > 0) tmp.Height = 40 - ((420 - windowframe.Height) / 6);
+            if (40 - (210 - windowframe.Width) > 0) tmp.Width = 40 - ((210 - windowframe.Width) / 3);
+            if (100 - (420 - windowframe.Height) > 0) txtPhone.Width = 100 - ((210 - windowframe.Width) / 3);
+
+            //Set dynamic fontsize
+            if (tmp.Width > tmp.Height) tmp.FontSize = 17 * tmp.Height / 40;
+            else tmp.FontSize = 17 * tmp.Width / 40;
+
+            //Set dynamic position on grid
+            Thickness margin = button1.Margin;
+
+            //Head
+            margin.Top = 40;
+            margin.Left = 20 + txtPhone.Width + 20;
+            bBackspace.Margin = margin;
+
+            //First row
+            margin.Left = 20;
+            margin.Top = 40 + tmp.Height + 20;
+            button1.Margin = margin;
+
+            margin.Left = 20 + tmp.Width + 20;
+            margin.Top = 40 + tmp.Height + 20;
+            button2.Margin = margin;
+
+            margin.Left = 3 * 20 + 2 * tmp.Width;
+            margin.Top = 40 + tmp.Height + 20;
+            button3.Margin = margin;
+
+            //Second row
+            margin.Left = 20;
+            margin.Top = 40 + 2 * tmp.Height + 2 * 20;
+            button4.Margin = margin;
+
+            margin.Left = 20 + tmp.Width + 20;
+            margin.Top = 40 + 2 * tmp.Height + 2 * 20;
+            button5.Margin = margin;
+
+            margin.Left = 3 * 20 + 2 * tmp.Width;
+            margin.Top = 40 + 2 * tmp.Height + 2 * 20;
+            button6.Margin = margin;
+
+            //Third row
+            margin.Left = 20;
+            margin.Top = 40 + 3 * tmp.Height + 3 * 20;
+            button7.Margin = margin;
+
+            margin.Left = 20 + tmp.Width + 20;
+            margin.Top = 40 + 3 * tmp.Height + 3 * 20;
+            button8.Margin = margin;
+
+            margin.Left = 3 * 20 + 2 * tmp.Width;
+            margin.Top = 40 + 3 * tmp.Height + 3 * 20;
+            button9.Margin = margin;
+
+            //Fourth row
+            margin.Left = 20;
+            margin.Top = 40 + 4 * tmp.Height + 4 * 20;
+            bStern.Margin = margin;
+
+            margin.Left = 20 + tmp.Width + 20;
+            margin.Top = 40 + 4 * tmp.Height + 4 * 20;
+            button0.Margin = margin;
+
+            margin.Left = 3 * 20 + 2 * tmp.Width;
+            margin.Top = 40 + 4 * tmp.Height + 4 * 20;
+            bHashtag1.Margin = margin;
+
+            //Fifth row
+            margin.Left = 20;
+            margin.Top = 40 + 5 * tmp.Height + 5 * 20;
+            bSave.Margin = margin;
+
+            margin.Left = 20 + tmp.Width + 20;
+            margin.Top = 40 + 5 * tmp.Height + 5 * 20;
+            bCall.Margin = margin;
+
+            margin.Left = 3 * 20 + 2 * tmp.Width;
+            margin.Top = 40 + 5 * tmp.Height + 5 * 20;
+            bSMS.Margin = margin;
+
+        }
+
     }
 }
