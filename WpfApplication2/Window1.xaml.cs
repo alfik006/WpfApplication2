852f355c464ce44e96467ca291da14a750f2f704
alfik006 <anneliesse15@gmail.com>
2015-11-02 22:50:10 +0100

    Skalierung von weiteren Fenstern
    noch nicht fertig

--- WpfApplication2/Window1.xaml.cs
+++ WpfApplication2/Window1.xaml.cs
@@ -49,5 +49,42 @@ namespace WpfApplication2
 
             txt_SMS.Text = newText;
         }
+
+        private void windowframe_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //Set dynamic object size
+            if (40 - (250 - windowframe.Height) > 0) tmp.Height = 30 - ((250 - windowframe.Height));
+            if (40 - (310 - windowframe.Width) > 0) tmp.Width = 60 - ((310 - windowframe.Width));
+            if (100 - (250 - windowframe.Height) > 0) txt_SMS.Width = 220 - ((310 - windowframe.Width));
+            if (100 - (250 - windowframe.Height) > 0) label1.Width = 220 - ((310 - windowframe.Width));
+
+            //Set dynamic fontsize
+            if (tmp.Width > tmp.Height) tmp.FontSize = 17 * tmp.Height / 60;
+            else tmp.FontSize = 17 * tmp.Width / 60;
+
+           //Set dynamic position on grid
+            Thickness margin = bBackspace.Margin;
+            Thickness margin2 = bBackspace.Margin;
+            Thickness margin3 = bBackspace.Margin;
+
+            //Head
+            margin.Top = 170;
+            margin.Left = 15;
+            bBackspace.Margin = margin;
+
+            margin.Top = 170;
+            margin.Left = 110;
+            bSMS.Margin = margin;
+
+            margin.Top = 170;
+            margin.Left = 180;
+            bCancel.Margin = margin;
+
+            margin.Top = 6;
+            margin.Left = 50;
+            txtPhone_W1.Margin = margin;
+
+        }
+
     }
 }
